b0faeeaa076b4c4c091b2cbf094d102d0693f34e
Ekyd <Ekyd777@gmail.com>
2022-07-25 12:12:56 +0300

    done task3

--- Task_3.py
+++ Task_3.py
@@ -3,15 +3,17 @@ import random
 
 
 def print_bord(desk: list):
-    for row in desk:
-        print(' '.join([str(i) for i in row]))
+    print("-" * 13)
+    for i in range(3):
+        print("|", desk[0 + i * 3], "|", desk[1 + i * 3], "|", desk[2 + i * 3], "|")
+        print("-" * 13)
 
 
 def lottery():
     user_input = None
     random_number = random.randint(0, 1)
     while user_input != 0 and user_input != 1:
-        user_input = int(input('Для жеребьевки выберите число 1 для игры "x" или 0 для игры "o": '))
+        user_input = int(input('Для жеребьевки выберите число 1 для игры "X" или 0 для игры "O": '))
 
     if random_number == user_input:
         return user_input
@@ -19,133 +21,60 @@ def lottery():
         return random_number
 
 
-def take_cross(number: int, game_bord: list):
-    cross = 'x'
-    zero = 'o'
-    if number == 1 and game_bord[0][0] != zero:
-        game_bord[0][0] = cross
-        return game_bord
-    elif number == 2 and game_bord[0][1] != zero:
-        game_bord[0][1] = cross
-        return game_bord
-    elif number == 3 and game_bord[0][2] != zero:
-        game_bord[0][2] = cross
-        return game_bord
-    elif number == 4 and game_bord[1][0] != zero:
-        game_bord[1][0] = cross
-        return game_bord
-    elif number == 5 and game_bord[1][1] != zero:
-        game_bord[1][1] = cross
-        return game_bord
-    elif number == 6 and game_bord[1][2] != zero:
-        game_bord[1][2] = cross
-        return game_bord
-    elif number == 7 and game_bord[2][0] != zero:
-        game_bord[2][0] = cross
-        return game_bord
-    elif number == 8 and game_bord[2][1] != zero:
-        game_bord[2][1] = cross
-        return game_bord
-    elif number == 9 and game_bord[2][2] != zero:
-        game_bord[2][2] = cross
-        return game_bord
+def take_input(player_token: str):
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token + "? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if 1 <= player_answer <= 9:
+            if str(board[player_answer - 1]) not in "XO":
+                board[player_answer - 1] = player_token
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
 
 
-def take_zero(number: int, game_bord: list):
-    cross = 'x'
-    zero = 'o'
-    if number == 1 and game_bord[0][0] != cross:
-        game_bord[0][0] = zero
-        return game_bord
-    elif number == 2 and game_bord[0][1] != cross:
-        game_bord[0][1] = zero
-        return game_bord
-    elif number == 3 and game_bord[0][2] != cross:
-        game_bord[0][2] = zero
-        return game_bord
-    elif number == 4 and game_bord[1][0] != cross:
-        game_bord[1][0] = zero
-        return game_bord
-    elif number == 5 and game_bord[1][1] != cross:
-        game_bord[1][1] = zero
-        return game_bord
-    elif number == 6 and game_bord[1][2] != cross:
-        game_bord[1][2] = zero
-        return game_bord
-    elif number == 7 and game_bord[2][0] != cross:
-        game_bord[2][0] = zero
-        return game_bord
-    elif number == 8 and game_bord[2][1] != cross:
-        game_bord[2][1] = zero
-        return game_bord
-    elif number == 9 and game_bord[2][2] != cross:
-        game_bord[2][2] = zero
-        return game_bord
+def check_win(game_board: list):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if game_board[each[0]] == game_board[each[1]] == game_board[each[2]]:
+            return game_board[each[0]]
+    return False
 
 
-def win_position(cross: bool, zero: bool):
-    if cross == True:
-        return 1
-    elif zero == True:
-        return 1
+def the_game(game_board):
+    position = lottery()
+    counter = position
+    if position == 1:
+        print('Первые ходят крестики')
     else:
-        return 0
+        print('Первые ходят нолики')
+    win = False
+    while not win:
+        print_bord(game_board)
+        if counter % 2 != 0:
+            take_input("X")
+        else:
+            take_input("O")
+        counter += 1
+        if counter > 4 - position:
+            tmp = check_win(game_board)
+            if tmp:
+                print(tmp, "выиграл!")
+                win = True
+                break
+        if counter - position == 9:
+            print("Ничья!")
+            break
+    print_bord(game_board)
 
 
-def win_cross(game_bord):
-    count = 0
-    for i in range(len(game_bord)):
-        for j in range(len(game_bord)):
-            if count < 3:
-                if game_bord[i][j] == 'x':
-                    count += 1
-            elif j == len(game_bord) - 1:
-                count = 0
-            else:
-                print('Победили крестики')
-                return True
-    return False
-
-
-def win_zero(game_bord):
-    count = 0
-    for i in range(len(game_bord)):
-        for j in range(len(game_bord)):
-            if count < 3:
-                if game_bord[i][j] == 'o':
-                    count += 1
-            elif j == len(game_bord) - 1:
-                count = 0
-            else:
-                print('Победили нолики')
-                return True
-    return False
-
-
-def the_game(user_turn: int):
-    board = [['.' for i in range(3)] for j in range(3)]
-    print_bord(board)
-    count = user_turn
-    result = 0
-
-    while result != 1:
-        if count % 2 != 0:
-            user_turn = int(input('Введите квадрат в который хотите поставить крестик (1-9): '))
-            board = take_cross(user_turn, board)
-            result = win_position(win_cross(board), win_zero(board))
-            print_bord(board)
-            count += 1
-        elif count % 2 == 0:
-            user_turn = int(input('Введите квадрат в который хотите поставить нолик (1-9): '))
-            board = take_zero(user_turn, board)
-            result = win_position(win_cross(board), win_zero(board))
-            print_bord(board)
-            count += 1
-
-
-position = lottery()
-if position == 1:
-    print('Первые ходят крестики')
-else:
-    print('Первые ходят нолики')
-the_game(position)
+board = list(range(1, 10))
+the_game(board)
+input("Нажмите Enter для выхода!")
